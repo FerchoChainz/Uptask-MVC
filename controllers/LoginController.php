552d9b66f42da4eddc7a3d0cc8f2a683ad0d66f5
FerchoChainz <a21110132@ceti.mx>
2025-12-18 19:36:41 -0600

    Login design done

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,21 +2,35 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController {
 
-    public static function login(){
+    public static function login(Router $router){
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         }
+
+
+        // render the view
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesion.'
+        ]);
     }
 
     public static function logout(){
 
     }
 
-    public static function create(){
-        echo 'desde crear';
+    public static function create(Router $router){
+        
+        
+        
+        // render the view
+        $router->render('auth/create', [
+            'titulo' => 'Crea tu cuenta.'
+        ]);
     }
 
     public static function recover(){

c80442231458d196986fa5e6bfaf7c4c9d4a4017
FerchoChainz <a21110132@ceti.mx>
2025-12-18 18:18:57 -0600

    login routing

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -5,6 +5,29 @@ namespace Controllers;
 class LoginController {
 
     public static function login(){
-        echo 'desde login';
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    }
+
+    public static function logout(){
+
+    }
+
+    public static function create(){
+        echo 'desde crear';
+    }
+
+    public static function recover(){
+        echo 'desde restablecer';
+    }
+
+    public static function message(){
+        echo 'desde mensaje';
+    }
+
+    public static function confirm(){
+        echo 'desde confirmar';
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,10 +2,30 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
-// Login
+// LOGIN
+$router->get('/',[LoginController::class,'login']);
+$router->post('/',[LoginController::class,'login']);
+$router->get('/logout',[LoginController::class,'login']);
+
+// CREATE ACCOUNT
+$router->get('/create',[LoginController::class, 'create']);
+$router->post('/create',[LoginController::class, 'create']);
+
+// FORGOT PASSWORD
+$router->get('/forget',[LoginController::class, 'forget']);
+$router->post('/forget',[LoginController::class, 'forget']);
+
+// RECOVER PASSWORD
+$router->get('/recover',[LoginController::class, 'recover']);
+$router->post('/recover',[LoginController::class, 'recover']);
+
+// CONFIRM ACCOUNT
+$router->get('/message',[LoginController::class, 'message']);
+$router->get('/confirm',[LoginController::class, 'confirm']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
